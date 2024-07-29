e5c0e22580920ada218bb7f07e3267d929396dff
Omer Fadlon <omerfad@post.bgu.ac.il>
2024-07-29 11:29:37 +0300

    Update ErrorCheck.java

--- World cup update subscription service/SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/api/ErrorCheck.java
+++ World cup update subscription service/SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/api/ErrorCheck.java
@@ -3,6 +3,7 @@ package bgu.spl.net.api;
 public class ErrorCheck {
     
     public static String connectError(String[] arrayOfMessage){
+        
         if(!arrayOfMessage[1].replaceAll("\\s", "").equals("accept-version:1.2"))
             return "Didn't contain a version header or wrong version";
         if(!arrayOfMessage[2].replaceAll("\\s", "").equals("host:stomp.cs.bgu.ac.il"))
@@ -15,12 +16,14 @@ public class ErrorCheck {
             return "Didn't contain a passcode header";       
         return "";
     }
+    
     public static String sendError(String[] arrayOfMessage){
         String  [] dest=arrayOfMessage[1].split(":",0);
         if(!dest[0].replaceAll("\\s", "").equals("destination"))
             return "Didn't contain a destination header";
         return "";
     }
+    
     public static String subscribeError(String[] arrayOfMessage){
         String  [] dest=arrayOfMessage[1].split(":",0);
         if(!dest[0].replaceAll("\\s", "").equals("destination"))
@@ -30,12 +33,14 @@ public class ErrorCheck {
                 return "Didn't contain an id header";
         return "";
     }
+    
     public static String unsubscribeError(String[] arrayOfMessage){
         String  [] id=arrayOfMessage[1].split(":",0);
         if(!id[0].replaceAll("\\s", "").equals("id"))
             return "Didn't contain an id header";
         return "";
     }
+    
     public static String disconnectError(String[] arrayOfMessage){
         String  [] receipt=arrayOfMessage[1].split(":",0);
         if(!receipt[0].replaceAll("\\s", "").equals("receipt"))
